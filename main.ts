63abf385fb39fc00d3e6f93e29a6dc4913833f7a
latrellBaptista <72996921+latrellBaptista@users.noreply.github.com>
2020-10-21 20:53:51 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,56 @@
+scene.setBackgroundColor(14)
+effects.confetti.startScreenEffect()
+let Randy = sprites.create(img`
+    . . . . f f f f . . . . . 
+    . . f f f f f f f f . . . 
+    . f f f f f f c f f f . . 
+    f f f f f f c c f f f c . 
+    f f f c f f f f f f f c . 
+    c c c f f f e e f f c c . 
+    f f f f f e e f f c c f . 
+    f f f b f e e f b f f f . 
+    . f 4 1 f 4 4 f 1 4 f . . 
+    . f e 4 4 4 4 4 4 e f . . 
+    . f f f e e e e f f f . . 
+    f e f b 7 7 7 7 b f e f . 
+    e 4 f 7 7 7 7 7 7 f 4 e . 
+    e e f 6 6 6 6 6 6 f e e . 
+    . . . f f f f f f . . . . 
+    . . . f f . . f f . . . . 
+    `, SpriteKind.Player)
+Randy.setPosition(80, 104)
+let jackson = sprites.create(img`
+    . . . . f f f f . . . . 
+    . . f f e e e e f f . . 
+    . f f e e e e e e f f . 
+    f f f f 4 e e e f f f f 
+    f f f 4 4 4 e e f f f f 
+    f f f 4 4 4 4 e e f f f 
+    f 4 e 4 4 4 4 4 4 e 4 f 
+    f 4 4 f f 4 4 f f 4 4 f 
+    f e 4 4 4 4 4 4 4 4 e f 
+    . f e 4 4 1 1 4 4 e f . 
+    . f f e 4 4 4 4 e f f . 
+    e 4 f b 1 1 1 1 b f 4 e 
+    4 d f 1 1 1 1 1 1 f d 4 
+    4 4 f 6 6 6 6 6 6 f 4 4 
+    . . . f f f f f f . . . 
+    . . . f f . . f f . . . 
+    `, SpriteKind.Player)
+jackson.setPosition(80, 39)
+jackson.say("Hey man , wassup")
+pause(500)
+Randy.say("Hey , nothing")
+pause(5000)
+jackson.say("Did you do mazzone's homework")
+pause(1000)
+Randy.say("Yea I did ")
+pause(2000)
+jackson.say("Can you give it to me ;)")
+pause(2000)
+Randy.say("I cant man")
+pause(5000)
+jackson.say("why not? ")
+pause(2000)
+Randy.say("I cant share codes 😂")
+effects.clouds.endScreenEffect()
